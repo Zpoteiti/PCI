9fb5cdf31547a7d12797e099252fa2e128b6de79
P-PPT <76959908+PT-VU@users.noreply.github.com>
2023-06-16 05:47:44 +0800

    bonus

---
+++ assignment 1/Aggregation_bonus.py
@@ -0,0 +1,267 @@
+import random
+from enum import Enum, auto
+import pygame as pg
+from pygame.math import Vector2
+from vi import Agent, Simulation
+import math
+from vi.config import Config, dataclass, deserialize
+
+
+class AggregationConfig(Config):
+    site = 0
+    time = 0
+
+
+class Cockroach(Agent):
+    config: AggregationConfig
+
+    max_join_time: float = 101
+    max_still_time: float = 800
+    safe_distance: float = 5
+    max_velocity: float = 50
+    # s_noise = 0
+    state = "join"
+    join_time = 0
+    still_time = 0
+    # join_e = 0.2
+    # join_max = 0.8
+    # leave_chance = 0
+    # patience = 200
+    site_change = False
+    site_num = 0
+
+    def separation(self, neighbors):
+        # Separation to avoid overlapping
+        pos = [agent.pos for agent, len in neighbors]
+        drift = Vector2(0, 0)
+        for neighbor in pos:
+            diff = self.pos - neighbor
+            if diff.length() < self.safe_distance:
+                a = diff[0]
+                b = diff[1]
+                x = a * diff.length() / self.safe_distance
+                y = b * diff.length() / self.safe_distance
+                quotient = Vector2(a - x, b - y)
+                drift += quotient
+        return drift
+
+    # joining the group and return a random drift
+    def join(self):
+        self.change_image(1)
+        self.join_time += 1
+        neighbors = list(self.in_proximity_accuracy())
+        list_js = []
+        for agent, length in neighbors:
+            if agent.state == "still" or agent.state == "join":
+                list_js.append(agent)
+        neighbors = list_js
+        if len(neighbors) > 0:
+            debuff = 1 / len(neighbors)
+        else:
+            debuff = 0
+
+        buff = self.join_time / 100
+
+        # drift = -0.7 * self.move - self.move * (debuff * 3 - buff)
+        drift = Vector2(0,0)
+        # if there's more still and join neighbors less speed it gets
+        # if drift.length() > (self.move * -1).length():
+        #     drift = self.move * -0.7
+        if len(neighbors) > 3 and self.join_time > self.max_join_time - 100 * len(neighbors):
+            self.state = "still"
+            self.join_time = 0
+        if len(neighbors) < 4 and self.join_time > self.max_join_time:
+            self.state = "still"
+            self.join_time = 0
+
+        return drift
+
+    # leaving the group and return a random drift
+    def leave(self):
+        # self.change_image(2)
+        neighbors = list(self.in_proximity_accuracy())
+        # adding a random drift between (-s_noise, -s_noise) and (s_noise, s_noise) to the movement when leaving the group
+        # if self.move.length() < self.config.movement_speed * 0.5:
+            #self.move = Vector2(random.random() - 0.5, random.random() - 0.5).normalize() * self.config.movement_speed
+        avg_pos = Vector2(0,0)
+        num_s = 0
+        if len(neighbors) > 0:
+            for agent, length in neighbors:
+                if agent.state == "still":
+                    avg_pos += agent.pos
+                    num_s += 1
+            if num_s != 0:
+                avg_pos = (avg_pos) / (num_s)
+                self.move = (self.pos - avg_pos).normalize() * self.config.movement_speed
+            else:
+                self.move = Vector2(random.random() - 0.5, random.random() - 0.5).normalize() * self.config.movement_speed
+        else:
+            self.move = Vector2(random.random() - 0.5, random.random() - 0.5).normalize() * self.config.movement_speed
+
+        drift = Vector2(0, 0)
+
+        # if the agent is on the site, change state to wandering, in order to prevent the agent from stuck in group canter
+        # if not self.on_site():
+        #     self.state = "wdr"
+
+        return drift
+
+    # wandering around
+    def wandering(self):
+        # random drift
+        self.change_image(0)
+        # adding a random drift between (-s_noise, -s_noise) and (s_noise, s_noise) to the movement when wandering
+        if self.move.length() < self.config.movement_speed * 0.5:
+            self.move = Vector2(random.random() - 0.5, random.random() - 0.5).normalize() * self.config.movement_speed
+
+    # stay at the same position
+    def still(self):
+        self.change_image(3)
+        self.move = Vector2(0, 0)
+        return Vector2(0, 0)
+
+    def if_leave(self):
+
+        # x is the calculated additonal probability of leaving the group
+        x = 0
+        # count number of neighbours with state 'still'
+        num_s = 0
+        for agent, lenth in self.in_proximity_accuracy():
+            if agent.state == 'still':
+                num_s += 1
+        # count number of neighbours with state 'join'
+        num_j = 0
+        for agent, lenth in self.in_proximity_accuracy():
+            if agent.state == 'join':
+                num_s += 1
+
+        # count number of neighbours with state 'leave'
+        num_l = 0
+        for agent, lenth in self.in_proximity_accuracy():
+            if agent.state == 'leave':
+                num_s += 1
+
+        num_w = 0
+        for agent, lenth in self.in_proximity_accuracy():
+            if agent.state == 'wdr':
+                num_w += 1
+
+        # if not self.on_site():
+        #     self.state = 'wdr'
+        #     self.leave_chance = 0
+        #     return False
+
+        if len(list(self.in_proximity_accuracy())) > 0 and num_s < 3:
+            self.still_time += 1
+            if self.still_time > self.max_still_time:
+                x = 1
+            else:
+                x = 0
+
+        x += num_l + num_w - num_j - num_s
+
+        if len(list(self.in_proximity_accuracy())) > 0 and num_s > 3:
+            x = 1 / num_s ** 4
+
+        else:
+            self.still_time += 1
+            if self.still_time > self.max_still_time:
+                x = 1
+            else:
+                x = 0
+
+        # make a random number between 0 and 1
+        y = random.random()
+        # if y is smaller than x, then leave
+        if y < x:
+            return True
+        else:
+            return False
+
+    def count(self):
+        with open("test.txt", "a") as f:
+            f.write(f"{self.config.site},{self.config.time / 50}\n")
+
+    def change_position(self):
+        self.config.time += 1
+        #print(self.state)
+        if not (self.site_change) and self.state == "still":
+            self.site_change = True
+            self.config.site += 1
+            print("+1")
+            self.count()
+
+        elif self.site_change and not(self.state == "still"):
+            self.site_change = False
+            self.config.site -= 1
+            print("-1")
+            self.count()
+
+        neighbors = list(self.in_proximity_accuracy())
+        drift = Vector2(0, 0)
+
+        self.there_is_no_escape()
+        # Obstacle Avoidance
+        obstacle_hit = pg.sprite.spritecollideany(self, self._obstacles, pg.sprite.collide_mask)  # type: ignore
+        collision = bool(obstacle_hit)
+
+        # Reverse direction when colliding with an obstacle.
+        if collision:
+            self.move.rotate_ip(90)
+
+        # when wandering and hit sites
+        if self.state == "wdr":  # and self.if_join():
+            self.state = "join"
+        # if do not want to join, then bounce back
+        # if not (self.on_site()) and self.state == "join":  # and not self.if_join():
+        #     self.state = "wdr"
+            # self.move = Vector2(-self.move[0], -self.move[1])
+
+        # when still but want to leave
+        if self.state == 'still' and self.if_leave():
+            self.leave()
+            self.state = "join"
+
+        # if self.state == "leave":
+        #     self.state = "join"
+        # state decided and change position
+        if self.state == "join":
+            drift = self.join()
+        # elif self.state == "leave":
+            # drift = self.leave()
+        # elif self.state == "wdr":
+        #     self.wandering()
+        elif self.state == "still":
+            drift = self.still()
+
+        # self.move += drift
+
+        # make sure agents won't move too fast
+        if self.move.length() > self.max_velocity:
+            self.move = self.move.normalize() * self.max_velocity
+
+        # move
+        self.pos += self.move + self.separation(neighbors) + drift
+
+
+(
+    Simulation(
+        AggregationConfig(
+            image_rotation=False,
+            visualise_chunks=False,
+            movement_speed=7,
+            radius=50,
+            seed=1)
+    )
+    # spawn screen's edge
+    # .spawn_obstacle("images/obstacle.png", x=375, y=375)
+    # spawn sites
+    # .spawn_site("images/triangle@200px.png", x=200, y=300)
+    # .spawn_site("images/triangle@200px.png", x=500, y=300)
+    # spawn agents
+    .batch_spawn_agents(100, Cockroach,
+                        images=["images/white.png", 'images/red.png', 'images/blue.png', 'images/green.png'])
+
+    # run simulation
+    .run()
+)
