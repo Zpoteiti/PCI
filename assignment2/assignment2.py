974f2f782e895aa1cc027e73ee63970d4c865ea4
P-PPT <76959908+PT-VU@users.noreply.github.com>
2023-06-20 22:04:20 +0800

    introduced Polars

--- assignment2/assignment2.py
+++ assignment2/assignment2.py
@@ -5,6 +5,7 @@ from pygame.math import Vector2
 from vi import Agent, Simulation,TimeMachine
 import matplotlib.pyplot as plt
 from vi.config import Config, dataclass, deserialize
+import polars as pl
 
 class NatureConfig(Config):
     rabbit_reproduction_rate = 0.001
@@ -17,33 +18,13 @@ class NatureConfig(Config):
     #below are parameters for counting population
     rabbit_num = 0
     fox_num = 0
-    round = ""
-    time = 0
-    population_history = [[20,5,0],[20,5,1]]# [rabbit,fox,time]
+
 
 # adding noise to the movement to avoid agents moving in a straight line and overlapping
 # range from -noise_strength to 0.noise_strength
 def noise():
     return Vector2(random.random()*2-1, random.random()*2-1)
 
-# draw the graph of population
-def show(population):
-    plt.style.use('seaborn-whitegrid')
-    time = []
-    fox = []
-    rabbit = []
-    for i in population:
-        time.append(i[2])
-        rabbit.append(i[0])
-        fox.append(i[1])
-    plt.plot(time, fox, '-', label='fox')
-    plt.plot(time, rabbit, '-', label='rabbit')
-
-    plt.title('population')
-    plt.xlabel('time')
-    plt.ylabel('population_number')
-
-    plt.pause(0.00000000000000001)
 
 
 
@@ -53,6 +34,9 @@ class Rabbit(Agent):
     #a parameter count the initial population
     count = True
 
+    def update(self):
+        self.save_data("type", "rabbit")
+        self.save_data("num", self.config.rabbit_num)
 
     # run away from the nearest fox at highest speed
     def run_away_from_fox(self):
@@ -62,24 +46,17 @@ class Rabbit(Agent):
             .first()
         )
         if fox is not None:
-            # move away from the fox    
+            # move away from the fox
             self.move = self.pos - fox[0].pos
             self.move.scale_to_length(self.speed)
 
     def change_position(self):
-        # a statement to count the initial population
-        if self.config.round == "fox":
-            self.config.round = "wait"
-        else:
-            self.config.round = "rabbit"
-
-        # a statement to count the time steps
         if self.count:
             self.count = False
             self.config.rabbit_num += 1
 
         self.there_is_no_escape()
-        
+
         # run away from the nearest fox at highest speed
         self.run_away_from_fox()
         # make sure the speed equal to max speed
@@ -93,32 +70,25 @@ class Rabbit(Agent):
         self.move += noise()
         self.pos += self.move
 
-        rabbit = self.config.rabbit_num
-        fox = self.config.fox_num
-        time = self.config.time
-        population = self.config.population_history
 
-        # if (rabbit != population[-1][0] or fox != population[-1][1]) and time > 1:
-        if time % 20 == 0 and time > 1:
-            self.config.population_history.append([rabbit,fox,time])
-            show(self.config.population_history)
 
-        #oprint(self.config.population_history)
-        # if (rabbit != population[-1][0] or fox != population[-1][1]) and time > 1:
-        #     show(self.config.population_history)
-        print("rabbit:", self.config.rabbit_num, "time",self.config.time)
-        
 
 class Fox(Agent):
     config: NatureConfig
-    round = 0
     speed = 2
     energy = NatureConfig.energy
-    # a parameter count the initial population
     count = True
+
+    def update(self):
+        self.save_data("type", "fox")
+        self.save_data("num", self.config.fox_num)
+
 
     # chasing the nearest rabbit at highest speed
     def chase_rabbit(self):
+        if self.count:
+            self.count = False
+            self.config.fox_num += 1
 
         rabbit = (
             self.in_proximity_accuracy()
@@ -145,18 +115,11 @@ class Fox(Agent):
             self.reproduce()
             self.config.fox_num += 1
 
-            
-    def change_position(self):
-        if self.config.round == "rabbit":
-            self.config.round = "fox"
-            self.config.time += 1
 
-        if self.count:
-            self.count = False
-            self.config.fox_num += 1
+    def change_position(self):
+
 
         self.there_is_no_escape()
-        self.round += 1
         self.energy -= 1
 
         if self.energy < 0 and self.config.energy_switch:
@@ -175,34 +138,39 @@ class Fox(Agent):
         # move
         self.move += noise()
         self.pos += self.move
-        rabbit = self.config.rabbit_num
-        fox = self.config.fox_num
-        time = self.config.time
-        population = self.config.population_history
 
-        # if (rabbit != population[-1][0] or fox != population[-1][1]) and time > 1 :
-        if time % 20 == 0 and time > 1:
-            self.config.population_history.append([rabbit,fox,time])
-            show(self.config.population_history)
-        # if (rabbit != population[-1][0] or fox != population[-1][1]) and time > 1:
-        #     show(self.config.population_history)
-        print("fox:", self.config.fox_num, "time",self.config.time)
-        #print(self.config.population_history)
-            
 
-(
-    Simulation(
-        NatureConfig(
-            image_rotation=False,
-            visualise_chunks=False,
-            movement_speed=1,
-            radius=50,
-            seed=2)
+
+
+def run() -> pl.DataFrame:
+    return\
+        (
+        Simulation(
+            NatureConfig(
+                image_rotation=False,
+                visualise_chunks=False,
+                movement_speed=1,
+                radius=50,
+                seed=2,
+                duration=2000)
+        )
+        # spawn agents
+        .batch_spawn_agents(20, Rabbit, images=["images/rabbit.png"])
+        .batch_spawn_agents(5, Fox, images=["images/fox.png"])
+
+        # run simulation
+        .run()
+        .snapshots
+        .groupby(["type", "frame"],maintain_order=True).max()
     )
-    # spawn agents
-    .batch_spawn_agents(20, Rabbit, images=["images/rabbit.png"])
-    .batch_spawn_agents(5, Fox, images=["images/fox.png"])
-    
-    # run simulation
-    .run()
-)
+
+
+
+
+if __name__ == '__main__':
+    df = run()
+    print(df)
+
+    #export to excel
+    df.write_excel('tt.xlsx')
+
